8d5f06ec460f6f2b733b4419bd01750c6d82a2c1
ataylorme <andrew@ataylor.me>
2018-07-05 12:27:15 -0700

    Cut down on variable definition in main gulpfile
    * Move constants to `gulp/constants.js`
    * Use `gulp-load-plugins` for `gulp-` modules

---
+++ gulp/constants.js
@@ -0,0 +1,47 @@
+/* eslint-env es6 */
+'use strict';
+
+// gulp plugins
+export const gulpPlugins = require('gulp-load-plugins')();
+
+// Theme config
+let config = require('../dev/config/themeConfig.js');
+let themeConfig = config.theme;
+
+// Project paths
+export const paths = {
+	config: {
+		cssVars: './dev/config/cssVariables.json',
+		themeConfig: './dev/config/themeConfig.js'
+	},
+	php: {
+		src: ['dev/**/*.php', '!dev/optional/**/*.*'],
+		dest: './'
+	},
+	styles: {
+		src: ['dev/**/*.css', '!dev/optional/**/*.*'],
+		dest: './',
+		sass: ['dev/**/*.scss']
+	},
+	scripts: {
+		src: ['dev/**/*.js', '!dev/**/*.min.js', '!dev/js/libs/**/*.js', '!dev/optional/**/*.*', '!dev/config/**/*'],
+		min: 'dev/**/*.min.js',
+		dest: './',
+		libs: 'dev/js/libs/**/*.js',
+		libsDest: './js/libs/',
+		verboseLibsDest: './verbose/js/libs/'
+	},
+	images: {
+		src: ['dev/**/*.{jpg,JPG,png,svg}', '!dev/optional/**/*.*'],
+		dest: './'
+	},
+	languages: {
+		src: ['./**/*.php', '!dev/**/*.php', '!verbose/**/*.php'],
+		dest: './languages/' + config.theme.name + '.pot'
+	},
+	verbose: './verbose/',
+	export: {
+		src: ['**/*', '!' + config.theme.name, '!' + config.theme.name + '/**/*', '!dev/**/*', '!node_modules', '!node_modules/**/*', '!vendor', '!vendor/**/*', '!.*', '!composer.*', '!gulpfile.*', '!package*.*', '!phpcs.*', '!*.zip'],
+		dest: './'
+	}
+};
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,73 +6,22 @@
  * then run "gulp" in command line.
  */
 
-import gulp from 'gulp';
-import autoprefixer from 'autoprefixer';
-import browserSync from 'browser-sync';
-import cssnano from 'gulp-cssnano';
-import babel from 'gulp-babel';
-import eslint from 'gulp-eslint';
-import log from 'fancy-log';
-import gulpif from 'gulp-if';
-import image from 'gulp-image';
-import newer from 'gulp-newer';
-import partialImport from 'postcss-partial-import';
-import postcssPresetEnv from 'postcss-preset-env';
-import phpcs from 'gulp-phpcs';
-import postcss from 'gulp-postcss';
-import print from 'gulp-print';
-import replace from 'gulp-string-replace';
-import requireUncached from 'require-uncached';
-import sass from 'gulp-sass';
-import sourcemaps from 'gulp-sourcemaps';
-import sort from 'gulp-sort';
-import tabify from 'gulp-tabify';
-import uglify from 'gulp-uglify';
-import wppot from 'gulp-wp-pot';
-import zip from 'gulp-zip';
+ // External dependencies
+ import {src, dest, watch as gulpWatch, parallel, series} from 'gulp';
+ import autoprefixer from 'autoprefixer';
+ import browserSync from 'browser-sync';
+ import log from 'fancy-log';
+ import partialImport from 'postcss-partial-import';
+ import postcssPresetEnv from 'postcss-preset-env';
+ import requireUncached from 'require-uncached';
+
+// Internal dependencies
+import {paths, gulpPlugins} from './gulp/constants';
 
 // Import theme-specific configurations.
-var config = require('./dev/config/themeConfig.js');
-var themeConfig = config.theme;
+let config = require('./dev/config/themeConfig.js');
+let themeConfig = config.theme;
 themeConfig.isFirstRun = true;
-
-// Project paths
-const paths = {
-	config: {
-		cssVars: './dev/config/cssVariables.json',
-		themeConfig: './dev/config/themeConfig.js'
-	},
-	php: {
-		src: ['dev/**/*.php', '!dev/optional/**/*.*'],
-		dest: './'
-	},
-	styles: {
-		src: ['dev/**/*.css', '!dev/optional/**/*.*'],
-		dest: './',
-		sass: ['dev/**/*.scss']
-	},
-	scripts: {
-		src: ['dev/**/*.js', '!dev/**/*.min.js', '!dev/js/libs/**/*.js', '!dev/optional/**/*.*', '!dev/config/**/*'],
-		min: 'dev/**/*.min.js',
-		dest: './',
-		libs: 'dev/js/libs/**/*.js',
-		libsDest: './js/libs/',
-		verboseLibsDest: './verbose/js/libs/'
-	},
-	images: {
-		src: ['dev/**/*.{jpg,JPG,png,svg}', '!dev/optional/**/*.*'],
-		dest: './'
-	},
-	languages: {
-		src: ['./**/*.php', '!dev/**/*.php', '!verbose/**/*.php'],
-		dest: './languages/' + config.theme.name + '.pot'
-	},
-	verbose: './verbose/',
-	export: {
-		src: ['**/*', '!' + config.theme.name, '!' + config.theme.name + '/**/*', '!dev/**/*', '!node_modules', '!node_modules/**/*', '!vendor', '!vendor/**/*', '!.*', '!composer.*', '!gulpfile.*', '!package*.*', '!phpcs.*', '!*.zip'],
-		dest: './'
-	}
-};
 
 /**
  * Conditionally set up BrowserSync.
@@ -128,20 +77,20 @@ export function php() {
 		themeConfig.isFirstRun = false;
 	}
 
-	return gulp.src(paths.php.src)
+	return src(paths.php.src)
 	// If not a rebuild, then run tasks on changed files only.
-	.pipe(gulpif(!isRebuild, newer(paths.php.dest)))
-	.pipe(phpcs({
+	.pipe(gulpPlugins.if(!isRebuild, gulpPlugins.newer(paths.php.dest)))
+	.pipe(gulpPlugins.phpcs({
 		bin: 'vendor/bin/phpcs',
 		standard: 'WordPress',
 		warningSeverity: 0
 	}))
 	// Log all problems that was found
-	.pipe(phpcs.reporter('log'))
-	.pipe(replace('wprig', config.theme.slug))
-	.pipe(replace('WP Rig', config.theme.name))
-	.pipe(gulp.dest(paths.verbose))
-	.pipe(gulp.dest(paths.php.dest));
+	.pipe(gulpPlugins.phpcs.reporter('log'))
+	.pipe(gulpPlugins.stringReplace('wprig', config.theme.slug))
+	.pipe(gulpPlugins.stringReplace('WP Rig', config.theme.name))
+	.pipe(dest(paths.verbose))
+	.pipe(dest(paths.php.dest));
 
 }
 
@@ -149,12 +98,12 @@ export function php() {
  * Sass, if that's being used.
  */
 export function sassStyles() {
-	return gulp.src(paths.styles.sass, { base: "./" })
-	.pipe(sourcemaps.init())
-	.pipe(sass().on('error', sass.logError))
-	.pipe(tabify(2, true))
-	.pipe(sourcemaps.write('./maps'))
-	.pipe(gulp.dest('.'));
+	return src(paths.styles.sass, { base: "./" })
+	.pipe(gulpPlugins.sourcemaps.init())
+	.pipe(gulpPlugins.sass().on('error', gulpPlugins.sass.logError))
+	.pipe(gulpPlugins.tabify(2, true))
+	.pipe(gulpPlugins.sourcemaps.write('./maps'))
+	.pipe(dest('.'));
 }
 
 /**
@@ -164,18 +113,18 @@ export function styles() {
 	config = requireUncached('./dev/config/themeConfig.js');
 
 	// Reload cssVars every time the task runs.
-	let cssVars = requireUncached(paths.config.cssVars)
+    let cssVars = requireUncached(paths.config.cssVars);
 
-	return gulp.src(paths.styles.src)
-	.pipe(print())
-	.pipe(phpcs({
+	return src(paths.styles.src)
+	// .pipe(gulpPlugins.print())
+	.pipe(gulpPlugins.phpcs({
 		bin: 'vendor/bin/phpcs',
 		standard: 'WordPress',
 		warningSeverity: 0
 	}))
 	// Log all problems that was found
-	.pipe(phpcs.reporter('log'))
-	.pipe(postcss([
+	.pipe(gulpPlugins.phpcs.reporter('log'))
+	.pipe(gulpPlugins.postcss([
 		postcssPresetEnv({
 			stage: 3,
 			browsers: config.dev.browserslist,
@@ -191,11 +140,11 @@ export function styles() {
 			}
 		})
 	]))
-	.pipe(replace('wprig', config.theme.slug))
-	.pipe(replace('WP Rig', config.theme.name))
-	.pipe(gulp.dest(paths.verbose))
-	.pipe(gulpif(!config.dev.debug.styles, cssnano()))
-	.pipe(gulp.dest(paths.styles.dest));
+	.pipe(gulpPlugins.stringReplace('wprig', config.theme.slug))
+	.pipe(gulpPlugins.stringReplace('WP Rig', config.theme.name))
+	.pipe(dest(paths.verbose))
+	.pipe(gulpPlugins.if(!config.dev.debug.styles, gulpPlugins.cssnano()))
+	.pipe(dest(paths.styles.dest));
 }
 
 
@@ -204,16 +153,16 @@ export function styles() {
  */
 export function scripts() {
 	config = requireUncached('./dev/config/themeConfig.js');
-	return gulp	.src(paths.scripts.src)
-	.pipe(newer(paths.scripts.dest))
-	.pipe(eslint())
-	.pipe(eslint.format())
-	.pipe(babel())
-	.pipe(gulp.dest(paths.verbose))
-	.pipe(gulpif(!config.dev.debug.scripts, uglify()))
-	.pipe(replace('wprig', config.theme.slug))
-	.pipe(replace('WP Rig', config.theme.name))
-	.pipe(gulp.dest(paths.scripts.dest));
+	return src(paths.scripts.src)
+	.pipe(gulpPlugins.newer(paths.scripts.dest))
+	.pipe(gulpPlugins.eslint())
+	.pipe(gulpPlugins.eslint.format())
+	.pipe(gulpPlugins.babel())
+	.pipe(dest(paths.verbose))
+	.pipe(gulpPlugins.if(!config.dev.debug.scripts, gulpPlugins.uglify()))
+	.pipe(gulpPlugins.stringReplace('wprig', config.theme.slug))
+	.pipe(gulpPlugins.stringReplace('WP Rig', config.theme.name))
+	.pipe(dest(paths.scripts.dest));
 }
 
 
@@ -221,10 +170,10 @@ export function scripts() {
  * Copy JS libraries without touching them.
  */
 export function jsLibs() {
-	return gulp.src(paths.scripts.libs)
-	.pipe(newer(paths.scripts.verboseLibsDest))
-	.pipe(gulp.dest(paths.scripts.verboseLibsDest))
-	.pipe(gulp.dest(paths.scripts.libsDest));
+	return src(paths.scripts.libs)
+	.pipe(gulpPlugins.newer(paths.scripts.verboseLibsDest))
+	.pipe(dest(paths.scripts.verboseLibsDest))
+	.pipe(dest(paths.scripts.libsDest));
 }
 
 
@@ -232,20 +181,20 @@ export function jsLibs() {
  * Copy minified JS files without touching them.
  */
 export function jsMin() {
-	return gulp.src(paths.scripts.min)
-	.pipe(newer(paths.scripts.dest))
-	.pipe(gulp.dest(paths.verbose))
-	.pipe(gulp.dest(paths.scripts.dest));
+	return src(paths.scripts.min)
+	.pipe(gulpPlugins.newer(paths.scripts.dest))
+	.pipe(dest(paths.verbose))
+	.pipe(dest(paths.scripts.dest));
 }
 
 /**
  * Optimize images.
  */
 export function images() {
-	return gulp.src(paths.images.src)
-	.pipe(newer(paths.images.dest))
-	.pipe(image())
-	.pipe(gulp.dest(paths.images.dest));
+	return src(paths.images.src)
+	.pipe(gulpPlugins.newer(paths.images.dest))
+	.pipe(gulpPlugins.image())
+	.pipe(dest(paths.images.dest));
 }
 
 
@@ -253,22 +202,22 @@ export function images() {
  * Watch everything
  */
 export function watch() {
-	gulp.watch(paths.php.src, gulp.series(php, reload));
-	gulp.watch(paths.config.themeConfig, gulp.series(php, reload));
-	gulp.watch(paths.config.cssVars, gulp.series(styles, reload));
-	gulp.watch(paths.styles.sass, sassStyles);
-	gulp.watch(paths.styles.src, gulp.series(styles, reload));
-	gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
-	gulp.watch(paths.scripts.min, gulp.series(jsMin, reload));
-	gulp.watch(paths.scripts.libs, gulp.series(jsLibs, reload));
-	gulp.watch(paths.images.src, gulp.series(images, reload));
+	gulpWatch(paths.php.src, series(php, reload));
+	gulpWatch(paths.config.themeConfig, series(php, reload));
+	gulpWatch(paths.config.cssVars, series(styles, reload));
+	gulpWatch(paths.styles.sass, sassStyles);
+	gulpWatch(paths.styles.src, series(styles, reload));
+	gulpWatch(paths.scripts.src, series(scripts, reload));
+	gulpWatch(paths.scripts.min, series(jsMin, reload));
+	gulpWatch(paths.scripts.libs, series(jsLibs, reload));
+	gulpWatch(paths.images.src, series(images, reload));
 }
 
 
 /**
  * Map out the sequence of events on first load:
  */
-const firstRun = gulp.series(php, gulp.parallel(scripts, jsMin, jsLibs), sassStyles, styles, images, serve, watch);
+const firstRun = series(php, parallel(scripts, jsMin, jsLibs), sassStyles, styles, images, serve, watch);
 
 
 /**
@@ -281,15 +230,15 @@ export default firstRun;
  * Generate translation files.
  */
 export function translate() {
-	return gulp.src(paths.languages.src)
-	.pipe(sort())
-	.pipe(wppot({
+	return src(paths.languages.src)
+	.pipe(gulpPlugins.sort())
+	.pipe(gulpPlugins.wpPot({
 		domain: config.theme.name,
 		package: config.theme.name,
 		bugReport: config.theme.name,
 		lastTranslator: config.theme.author
 	}))
-	.pipe(gulp.dest(paths.languages.dest));
+	.pipe(dest(paths.languages.dest));
 }
 
 
@@ -297,22 +246,22 @@ export function translate() {
  * Create zip archive from generated theme files.
  */
 export function bundle() {
-	return gulp.src(paths.export.src)
-	.pipe(print())
-	.pipe(gulpif(config.export.compress, zip(config.theme.name + '.zip'), gulp.dest(paths.export.dest + config.theme.name)))
-	.pipe(gulpif(config.export.compress, gulp.dest(paths.export.dest)));
+	return src(paths.export.src)
+	// .pipe(gulpPlugins.print())
+	.pipe(gulpPlugins.if(config.export.compress, gulpPlugins.zip(config.theme.name + '.zip'), dest(paths.export.dest + config.theme.name)))
+	.pipe(gulpPlugins.if(config.export.compress, dest(paths.export.dest)));
 }
 
 
 /**
  * Test the theme.
  */
-const testTheme = gulp.series(php);
+const testTheme = series(php);
 
 
 /**
  * Export theme for distribution.
  */
-const bundleTheme = gulp.series(testTheme, gulp.parallel(scripts, jsMin, jsLibs), styles, images, translate, bundle);
+const bundleTheme = series(testTheme, parallel(scripts, jsMin, jsLibs), styles, images, translate, bundle);
 
 export { testTheme, bundleTheme };
