879c84e8a0d61d493ab643f48614b795868f35eb
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-07-17 18:24:20 +0200

    Do not initialize the theme if its requirements are not met, and inform the user with a warning.

--- dev/functions.php
+++ dev/functions.php
@@ -2,10 +2,23 @@
 /**
  * WP Rig functions and definitions
  *
+ * This file must be parseable by PHP 5.2.
+ *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package wprig
  */
+
+define( 'WPRIG_MINIMUM_WP_VERSION', '4.5' );
+define( 'WPRIG_MINIMUM_PHP_VERSION', '7.0' );
+
+/**
+ * Bail if requirements are not met.
+ */
+if ( version_compare( $GLOBALS['wp_version'], WPRIG_MINIMUM_WP_VERSION, '<' ) || version_compare( phpversion(), WPRIG_MINIMUM_PHP_VERSION, '<' ) ) {
+	require get_template_directory() . '/inc/back-compat.php';
+	return;
+}
 
 /**
  * Setup and core features configuration.
