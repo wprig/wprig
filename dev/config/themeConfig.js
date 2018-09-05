deff341f815f3f80a368545bf237fe40afbf90f2
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-09-05 05:28:58 +0200

    Fix PHPCS error.

--- dev/config/themeConfig.js
+++ dev/config/themeConfig.js
@@ -2,8 +2,7 @@
 
 module.exports = {
 	theme: {
-		// The slug must only consist of lowercase letters, numbers and dashes
-		slug: 'wp-rig',
+		slug: 'wp-rig', // The slug must only consist of lowercase letters, numbers and dashes.
 		name: 'WP Rig',
 		author: 'Morten Rand-Hendriksen'
 	},
