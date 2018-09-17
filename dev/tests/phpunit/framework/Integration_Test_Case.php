90d7e5482c5c46347bf2b998d8521d27a8cf5695
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-09-17 12:51:50 +0200

    Add integration tests for added theme support.

--- dev/tests/phpunit/framework/Integration_Test_Case.php
+++ dev/tests/phpunit/framework/Integration_Test_Case.php
@@ -14,4 +14,16 @@ use WP_UnitTestCase;
  */
 class Integration_Test_Case extends WP_UnitTestCase {
 
+	/**
+	 * Tears down the environment after each test.
+	 */
+	public function tearDown() {
+
+		// Fix for core test suite removing 'html5' theme support after each test.
+		$html5_support = get_theme_support( 'html5' );
+		parent::tearDown();
+		if ( ! empty( $html5_support ) ) {
+			add_theme_support( 'html5', $html5_support[0] );
+		}
+	}
 }
