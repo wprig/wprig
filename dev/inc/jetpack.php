0fe79188e396612b8012b98b84c72eac580bd996
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-09-17 15:10:41 +0200

    Fix WordPress Coding Standards violations.

--- dev/inc/jetpack.php
+++ dev/inc/jetpack.php
@@ -17,7 +17,8 @@
 function wp_rig_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
 	add_theme_support(
-		'infinite-scroll', array(
+		'infinite-scroll',
+		array(
 			'container' => 'main',
 			'render'    => 'wp_rig_infinite_scroll_render',
 			'footer'    => 'page',
@@ -29,7 +30,8 @@ function wp_rig_jetpack_setup() {
 
 	// Add theme support for Content Options.
 	add_theme_support(
-		'jetpack-content-options', array(
+		'jetpack-content-options',
+		array(
 			'post-details' => array(
 				'stylesheet' => 'wp-rig-style',
 				'date'       => '.posted-on',
