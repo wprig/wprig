a912812e280fec2d1d46bf2165c1c42764d4f5a6
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2018-10-01 14:33:33 -0700

    Add Default Block Styles. Update links to handbook documentation for Gutenberg features.

--- dev/functions.php
+++ dev/functions.php
@@ -90,16 +90,22 @@ function wprig_setup() {
 	);
 
 	/**
+	 * Add support for default block styles.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/#default-block-styles
+	 */
+	add_theme_support( 'wp-block-styles' );
+	/**
 	 * Add support for wide aligments.
 	 *
-	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/
+	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/#wide-alignment
 	 */
 	add_theme_support( 'align-wide' );
 
 	/**
 	 * Add support for block color palettes.
 	 *
-	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/
+	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/#block-color-palettes
 	 */
 	add_theme_support( 'editor-color-palette', array(
 		array(
@@ -160,7 +166,7 @@ function wprig_setup() {
 	/**
 	 * Add support for font sizes.
 	 *
-	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/
+	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/#block-font-sizes
 	 */
 	add_theme_support( 'editor-font-sizes', array(
 		array(
