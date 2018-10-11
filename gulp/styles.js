68cbdc3eef8b84f2e135e04a1a4db34fac462685
ataylorme <andrew@ataylor.me>
2018-10-10 20:37:53 -0700

    Update gulp scripts task for new dev process
    Don't do string replacements in dev mode. Remove verbose file output. Inject CSS file changes rather than doing a full reload.

--- gulp/styles.js
+++ gulp/styles.js
@@ -10,6 +10,7 @@ import requireUncached from 'require-uncached';
 // Internal dependencies
 import {rootPath, paths, gulpPlugins, isProd} from './constants';
 import {getThemeConfig, getStringReplacementTasks} from './utils';
+import {server} from './browserSync';
 
 /**
 * CSS via PostCSS + CSSNext (includes Autoprefixer by default).
@@ -23,7 +24,10 @@ export default function styles(done) {
 
 	const beforeReplacement = [
 		src(paths.styles.src, {sourcemaps: true}),
-		// gulpPlugins.print()
+		gulpPlugins.newer({
+			dest: paths.styles.dest,
+			extra: [paths.config.themeConfig, paths.config.cssVars]
+		}),
 		gulpPlugins.phpcs({
 			bin: `${rootPath}/vendor/bin/phpcs`,
 			standard: 'WordPress',
@@ -60,20 +64,22 @@ export default function styles(done) {
 				}
 			]
 		}),
-		dest(paths.verbose),
 		gulpPlugins.if(
 			!config.dev.debug.styles,
 			gulpPlugins.cssnano()
 		),
+		server.stream({match: "**/*.css"}),
 		dest(paths.styles.dest, {sourcemaps: true}),
 	];
 
 	pump(
 		[].concat(
 			beforeReplacement,
+			// Only do string replacements when building for production
 			gulpPlugins.if(
 				isProd,
-				getStringReplacementTasks()
+				getStringReplacementTasks(),
+				[]
 			),
 			afterReplacement
 		),
