839598a21d1a9335ce39d8c6ec07177251911599
ataylorme <andrew@ataylor.me>
2018-10-10 20:38:55 -0700

    Update main gulpfile for new dev process changes
    Remove jsMin and jsLibs tasks. Run more tasks in parallel on initial start.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,36 +6,29 @@ import {parallel, series} from 'gulp';
 
 // Internal dependencies
 import bundle from './gulp/bundle';
-import {serve, reload} from './gulp/browserSync';
+import generateCert from './gulp/generateCert';
 import images from './gulp/images';
-import jsLibs from './gulp/jsLibs';
-import jsMin from './gulp/jsMin';
 import php from './gulp/php';
+import {reload, serve} from './gulp/browserSync';
 import sassStyles from './gulp/sassStyles';
 import scripts from './gulp/scripts';
 import styles from './gulp/styles';
 import translate from './gulp/translate';
 import watch from './gulp/watch';
-import generateCert from './gulp/generateCert';
 
 /**
  * Map out the sequence of events on first load and make it the default task
  */
-export const firstRun = series(php, parallel(scripts, jsMin, jsLibs), sassStyles, styles, images, serve, watch);
+export const firstRun = series( parallel(php, images, sassStyles, styles, scripts), serve, watch);
 
 export default firstRun;
 
 /**
- * Test the theme.
- */
-export const testTheme = series(php);
-
-/**
  * Export theme for distribution.
  */
-export const bundleTheme = series(testTheme, parallel(scripts, jsMin, jsLibs), styles, images, translate, bundle);
+export const bundleTheme = series(php, scripts, styles, images, translate, bundle);
 
 /**
  * Export all imported functions as tasks
  */
-export { bundle, generateCert, jsLibs, jsMin, php, sassStyles, scripts, styles, translate, watch };
+export { bundle, generateCert, images, php, sassStyles, scripts, styles, translate, watch };
