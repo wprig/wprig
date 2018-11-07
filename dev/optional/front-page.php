690d4ef5b4b51cd6ea45a6467f8705c0076cf094
Felix Arntz <felixarntz@google.com>
2018-11-07 10:22:09 -0800

    Fix coding standards violations.

--- dev/optional/front-page.php
+++ dev/optional/front-page.php
@@ -13,7 +13,7 @@ get_header();
  * Include the component stylesheet for the content.
  * This call runs only once on index and archive pages.
  * At some point, override functionality should be built in similar to the template part below.
- * 
+ *
  * Note: If this was already done it will be skipped.
  */
 wp_print_styles( array( 'wp-rig-content', 'wp-rig-front-page' ) );
