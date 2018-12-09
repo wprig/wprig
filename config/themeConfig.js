db79fc242827615775243353e22160d83adfe4b8
Josh Pollock <josh@calderawp.com>
2018-12-09 15:48:08 -0600

    merge custom and default configs from JSON #228

--- config/themeConfig.js
+++ config/themeConfig.js
@@ -1,35 +1,12 @@
 'use strict';
+const fs = require( 'fs' );
 
-module.exports = {
-	theme: {
-		slug: 'wp-rig', // The slug must only consist of lowercase letters, numbers and dashes.
-		name: 'WP Rig',
-		author: 'The WP Rig Contributors',
-		PHPNamespace: 'WP_Rig\\WP_Rig' // Backslashes must be escaped in JavaScript
-	},
-	dev: {
-		browserSync: {
-			live: true,
-			proxyURL: 'wprig.test:8888',
-			bypassPort: '8181',
+const custom = __dirname + '/themeConfigCustom.json';
+const defaultConfig = require(__dirname + '/themeConfigDefault.json' );
+if ( fs.existsSync( custom ) ) {
+	const merge = require( 'deepmerge' );
+	module.exports = merge(defaultConfig,require( custom ));
+} else {
+	module.exports = defaultConfig;
+}
 
-			// To use HTTPS you need a cert/key
-			// Please see the README for instructions
-			// keyPath: '',
-			// certPath: '',
-			https: false
-		},
-		browserslist: [ // See https://github.com/browserslist/browserslist
-			'> 1%',
-			'last 2 versions'
-		],
-		debug: {
-			styles: false, // Render verbose CSS for debugging.
-			scripts: false, // Render verbose JS for debugging.
-			phpcs: true // Code sniff PHP files
-		}
-	},
-	export: {
-		compress: true
-	}
-};
