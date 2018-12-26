a7debf2dc766726f332a05d9c512834f890a7e80
Rachel Cherry <rachel@bamadesigner@com>
2018-12-26 15:49:49 -0600

    Changing theme config file names

--- config/themeConfig.js
+++ config/themeConfig.js
@@ -1,8 +1,8 @@
 'use strict';
 const fs = require( 'fs' );
 
-const custom = __dirname + '/themeConfigCustom.json';
-const defaultConfig = require(__dirname + '/themeConfigDefault.json' );
+const custom = __dirname + '/config.json';
+const defaultConfig = require(__dirname + '/config.default.json' );
 if ( fs.existsSync( custom ) ) {
 	const merge = require( 'deepmerge' );
 	module.exports = merge(defaultConfig,require( custom ));
