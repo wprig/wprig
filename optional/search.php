ff1916800dbc4d70d5af9536405a5ebf9ebfd5ce
Benoît Chantre <mail@benoitchantre.com>
2019-01-12 00:23:56 +0100

    Use numerical pagination for search reusults

--- optional/search.php
+++ optional/search.php
@@ -23,7 +23,14 @@ get_header();
 				get_template_part( 'template-parts/content/entry', get_post_type() );
 			}
 
-			the_posts_navigation();
+			the_posts_pagination(
+				array(
+					'mid_size'           => 2,
+					'prev_text'          => _x( 'Previous', 'previous set of search results', 'wp-rig' ),
+					'next_text'          => _x( 'Next', 'next set of search results', 'wp-rig' ),
+					'screen_reader_text' => __( 'Search results navigation', 'wp-rig' ),
+				)
+			);
 		} else {
 			get_template_part( 'template-parts/content/error' );
 		}
