0a5e4e4196e18c72e22bfda037c1d3f125215ac1
Benoît Chantre <mail@benoitchantre.com>
2019-01-24 22:31:53 +0100

    Use the_posts_pagination() everywhere

--- template-parts/content/pagination.php
+++ template-parts/content/pagination.php
@@ -7,15 +7,11 @@
 
 namespace WP_Rig\WP_Rig;
 
-if ( is_search() ) {
-	the_posts_pagination(
-		array(
-			'mid_size'           => 2,
-			'prev_text'          => _x( 'Previous', 'previous set of search results', 'wp-rig' ),
-			'next_text'          => _x( 'Next', 'next set of search results', 'wp-rig' ),
-			'screen_reader_text' => __( 'Search results navigation', 'wp-rig' ),
-		)
-	);
-} else {
-	the_posts_navigation();
-}
+the_posts_pagination(
+	array(
+		'mid_size'           => 2,
+		'prev_text'          => _x( 'Previous', 'previous set of search results', 'wp-rig' ),
+		'next_text'          => _x( 'Next', 'next set of search results', 'wp-rig' ),
+		'screen_reader_text' => __( 'Page navigation', 'wp-rig' ),
+	)
+);
