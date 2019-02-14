828c1efe7b649cca81f02618dab5b740c1ae06c8
Benoît Chantre <mail@benoitchantre.com>
2019-02-14 19:29:27 +0100

    Use excerpts instead of full posts in search results (#252)
    * Enable search template by default
    
    Closes #251.
    
    * Add setting for search results
    
    Display Summary (default) or Full text.
    
    * Revert "Add setting for search results"
    
    This reverts commit c5c67386b9c27bb8050ac446fa01b61958eb0920.
    
    * Use excerpt for search results instead of full post
    
    * Typo
    
    * Revert "Enable search template by default"
    
    This reverts commit 2b6476753dc479cc768e4e1a29e76141e9a2635a.

--- template-parts/content/entry_header.php
+++ template-parts/content/entry_header.php
@@ -10,9 +10,13 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <header class="entry-header">
-	<?php get_template_part( 'template-parts/content/entry_title', get_post_type() ); ?>
+	<?php
+	get_template_part( 'template-parts/content/entry_title', get_post_type() );
 
-	<?php get_template_part( 'template-parts/content/entry_meta', get_post_type() ); ?>
+	get_template_part( 'template-parts/content/entry_meta', get_post_type() );
 
-	<?php get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() ); ?>
+	if ( ! is_search() ) {
+		get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
+	}
+	?>
 </header><!-- .entry-header -->
