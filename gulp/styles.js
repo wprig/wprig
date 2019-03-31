957fe1a7ef201e28e5168ec4e19e79129a1f2e6a
Andrew Taylor <ataylorme@users.noreply.github.com>
2019-03-31 11:29:41 -0700

    Use more PostCSS plugins (#396)
    ## Description
    [`gulp-cssnano`](https://github.com/ben-eb/gulp-cssnano) is no longer maintained.
    
    This uses [`cssnano`](https://cssnano.co/guides/getting-started/) as a `PostCSS` plugin, instead of relying on `gulp-cssnano` as an independent step in the pipeline.
    
    Removes [`gulp-stylelint`](npmjs.com/package/gulp-stylelint) in favor of the [stylelint PostCSS plugin](stylelint.io).
    
    Adds [`postcss-reporter`](npmjs.com/package/postcss-reporter) to surface Stylelint warnings.
    
    Fixes CSS issues surfaced by Stylelint.
    ## List of changes
    <!-- Please describe what was changed/added. -->
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    ## Checklist:
    - [ ] This pull request relates to a ticket.
    - [x] This code is tested.
    - [ ] This change has been added to CHANGELOG.md
    - [x] I want my code added to WP Rig.

--- gulp/styles.js
+++ gulp/styles.js
@@ -6,6 +6,9 @@ import {src, dest} from 'gulp';
 import postcssPresetEnv from 'postcss-preset-env';
 import AtImport from 'postcss-import';
 import pump from 'pump';
+import cssnano from 'cssnano';
+import stylelint from 'stylelint';
+import reporter from 'postcss-reporter';
 
 // Internal dependencies
 import {rootPath, paths, gulpPlugins, isProd} from './constants';
@@ -24,6 +27,46 @@ import {server} from './browserSync';
 export default function styles(done) {
 	const config = getThemeConfig();
 
+	const postcssPlugins = [
+		stylelint(),
+		AtImport({
+			path: [paths.styles.srcDir]
+		}),
+		postcssPresetEnv({
+			importFrom: (
+				configValueDefined('config.dev.styles.importFrom') ?
+				appendBaseToFilePathArray(config.dev.styles.importFrom, paths.styles.srcDir) :
+				[]
+			),
+			stage: (
+				configValueDefined('config.dev.styles.stage') ?
+				config.dev.styles.stage :
+				3
+			),
+			preserve: true,
+			features: (
+				configValueDefined('config.dev.styles.features') ?
+				config.dev.styles.features :
+				{
+					'custom-media-queries': true,
+					'custom-properties': true,
+					'nesting-rules': true
+				}
+			)
+		})
+	];
+
+	// Only minify if we aren't building for
+	// production and debug is not enabled
+	if( ! config.dev.debug.styles && ! isProd ) {
+		postcssPlugins.push(cssnano());
+	}
+
+	// Report messages from other postcss plugins
+	postcssPlugins.push(
+		reporter({ clearReportedMessages: true })
+	);
+
 	const beforeReplacement = [
 		src( paths.styles.src, {sourcemaps: !isProd} ),
 		logError('CSS'),
@@ -38,50 +81,10 @@ export default function styles(done) {
 		}),
 		// Log all problems that were found.
 		gulpPlugins.phpcs.reporter('log'),
-		gulpPlugins.postcss([
-			AtImport({
-				path: [paths.styles.srcDir]
-			}),
-			postcssPresetEnv({
-				importFrom: (
-					configValueDefined('config.dev.styles.importFrom') ?
-					appendBaseToFilePathArray(config.dev.styles.importFrom, paths.styles.srcDir) :
-					[]
-				),
-				stage: (
-					configValueDefined('config.dev.styles.stage') ?
-					config.dev.styles.stage :
-					3
-				),
-				preserve: true,
-				features: (
-					configValueDefined('config.dev.styles.features') ?
-					config.dev.styles.features :
-					{
-						'custom-media-queries': true,
-						'custom-properties': true,
-						'nesting-rules': true
-					}
-				)
-			})
-		]),
 	];
 
 	const afterReplacement = [
-		gulpPlugins.stylelint({
-			failAfterError: false,
-			fix: true,
-			reporters: [
-				{
-					formatter: 'string',
-					console: true
-				}
-			]
-		}),
-		gulpPlugins.if(
-			!config.dev.debug.styles,
-			gulpPlugins.cssnano()
-		),
+		gulpPlugins.postcss(postcssPlugins),
 		gulpPlugins.rename({
 			suffix: '.min'
 		}),
