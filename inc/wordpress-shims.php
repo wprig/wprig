855227c6a5a2dbed99ad5bd9ab34e13fe370d755
Benoît Chantre <mail@benoitchantre.com>
2019-04-04 22:16:44 +0200

    Indent with tabs

--- inc/wordpress-shims.php
+++ inc/wordpress-shims.php
@@ -9,11 +9,11 @@
  * Adds backwards compatibility for wp_body_open() introduced with WordPress 5.2
  */
 if ( ! function_exists( 'wp_body_open' ) ) {
-    /**
-     * Run the wp_body_open action.
-     *
-     * @return void
-     */
+	/**
+	 * Run the wp_body_open action.
+	 *
+	 * @return void
+	 */
 	function wp_body_open() {
 		do_action( 'wp_body_open' );
 	}
