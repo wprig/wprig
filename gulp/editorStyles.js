da798abdafdc23a9b6bc6d80b1a912ddca108701
Andrew Taylor <ataylorme@users.noreply.github.com>
2019-04-11 10:19:38 -0700

    Global preserve false for postcss-import in editor CSS (#419)

--- gulp/editorStyles.js
+++ gulp/editorStyles.js
@@ -59,6 +59,7 @@ export function editorStylesAfterReplacementStream() {
 				config.dev.styles.stage :
 				3
 			),
+			preserve: false,
 			features: (
 				configValueDefined('config.dev.styles.features') ?
 				config.dev.styles.features :
