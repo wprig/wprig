05be6255bb6965062a8e482fa19345e2c109850e
Benoît Chantre <mail@benoitchantre.com>
2019-05-10 13:19:43 +0200

    Fix i18n for site info
    Closes 478.

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -18,8 +18,8 @@ namespace WP_Rig\WP_Rig;
 	</a>
 	<span class="sep"> | </span>
 	<?php
-	/* translators: 1: Theme name, 2: Theme author. */
-	printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>', 'the contributors' );
+	/* translators: Theme name. */
+	printf( esc_html__( 'Theme: %s by the contributors.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>' );
 
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
