b24bb2af6216e006654ee9cb5f4ba44f47fec741
Benoît Chantre <mail@benoitchantre.com>
2019-07-08 21:48:45 +0200

    Remove unneeded code

--- assets/js/src/editor/editor-filters.js
+++ assets/js/src/editor/editor-filters.js
@@ -5,11 +5,6 @@
  */
 
 wp.domReady( function() {
-	wp.blocks.registerBlockStyle( 'core/list', {
-		name: 'default',
-		label: 'Regular',
-		isDefault: true
-	});
 
 	wp.blocks.registerBlockStyle( 'core/list', {
 		name: 'checkmark-list',
