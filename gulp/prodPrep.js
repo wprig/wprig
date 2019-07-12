34f1b2453f96cd0b64046e729af4e6fa332b80a5
Andrew Taylor <andrew@ataylor.me>
2019-07-11 21:44:51 -0700

    Formatting

--- gulp/prodPrep.js
+++ gulp/prodPrep.js
@@ -37,7 +37,7 @@ export default function prodPrep(done) {
 
     // The dev theme and the prod theme can't have the same name
     if (
-        process.env.JEST_WORKER_ID === undefined &&
+        typeof process.env.JEST_WORKER_ID === undefined &&
         isProd &&
         path.basename(prodThemePath) === path.basename(rootPath)
     ) {
