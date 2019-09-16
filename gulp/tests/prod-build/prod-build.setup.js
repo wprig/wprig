44def9b15e9a57e9663dc61d4af7aba247d56d55
Andrew Taylor <andrew@ataylor.me>
2019-09-16 05:45:26 -0400

    Restore existing files mocked during the gulp test process
    See #596. If a file already exists in the mock file destination rename it with a suffix of `-existing` during gulp prod test set up and rename it back to the original file name during gulp prod test cleanup.

--- gulp/tests/prod-build/prod-build.setup.js
+++ gulp/tests/prod-build/prod-build.setup.js
@@ -13,5 +13,14 @@ import { filesToMock } from './prod-build.utils';
 
 // Copy the mock files to their destination before testing.
 filesToMock.forEach( ( file ) => {
+	// Check if the file already exists
+	const fileExists = fs.existsSync( file.dest );
+	if ( fileExists ) {
+		// If it does, rename the file adding an -existing suffix
+		const existingFile = file.dest.replace( /(\.[\w\d_-]+)$/i, '-existing$1' );
+		fs.renameSync( file.dest, existingFile );
+	}
+
+	// Copy the lock file to the desired location
 	fs.copyFileSync( file.mock, file.dest );
 } );
