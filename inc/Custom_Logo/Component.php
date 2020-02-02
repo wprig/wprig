d6fcfdcc31a734aaff3ecd65d963e57819f8ec89
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-02-03 03:24:06 +1000

    Re-order custom logo args dimensions

--- inc/Custom_Logo/Component.php
+++ inc/Custom_Logo/Component.php
@@ -44,8 +44,8 @@ class Component implements Component_Interface {
 			apply_filters(
 				'wp_rig_custom_logo_args',
 				[
-					'height'      => 250,
 					'width'       => 250,
+					'height'      => 250,
 					'flex-width'  => false,
 					'flex-height' => false,
 				]
