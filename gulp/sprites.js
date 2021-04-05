88abe8253cc17e071d3fe074a075606b8cbb2e1f
Christopher Richardson <cwr@mirovoysales.com>
2021-04-05 18:57:12 +0200

    basic sprite support

---
+++ gulp/sprites.js
@@ -0,0 +1,47 @@
+/* eslint-env es6 */
+'use strict';
+
+/**
+ * External dependencies
+ */
+import { src, dest } from 'gulp';
+import pump from 'pump';
+
+/**
+ * Internal dependencies
+ */
+import { paths, gulpPlugins } from './constants';
+
+var config = {
+	shape: {
+		meta: paths.sprites.srcDir + '/meta.yaml'
+	},
+	svg: {
+		xmlDeclaration: false,
+		doctypeDeclaration: false,
+		namespaceIDs: true,
+		namespaceIDPrefix: 'wprig'
+	},
+	mode: {
+		symbol: {
+			dest: '.',
+			sprite: 'sprite.svg.php',
+			inline: true,
+			render: {css: true}
+		}
+	}
+};
+
+/**
+ * Compile svg images into sprite
+ * @param {function} done function to call when async processes finish
+ * @return {Stream} single stream
+ */
+export default function sprites( done ) {
+	return pump( [
+		src( paths.sprites.src ),
+		gulpPlugins.newer( paths.sprites.dest ),
+		gulpPlugins.svgSprite(config),
+		dest( paths.sprites.dest ),
+	], done );
+}
