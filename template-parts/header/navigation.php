693e4d89e3d05db86c8e00ad1bc330036705a016
robruiz <rob.c.ruiz@gmail.com>
2021-12-06 22:31:36 -0600

    Added better mobile nav using new hooks

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -13,7 +13,7 @@ if ( ! wp_rig()->is_primary_nav_menu_active() ) {
 
 ?>
 
-<nav id="site-navigation" class="main-navigation nav--toggle-sub nav--toggle-small" aria-label="<?php esc_attr_e( 'Main menu', 'wp-rig' ); ?>"
+<nav id="<?php echo apply_filters('wp_rig_site_navigation_id', 'site-navigation'); ?>" class="<?php echo apply_filters('wp_rig_site_navigation_classes', 'main-navigation nav--toggle-sub nav--toggle-small'); ?>" aria-label="<?php esc_attr_e( 'Main menu', 'wp-rig' ); ?>"
 	<?php
 	if ( wp_rig()->is_amp() ) {
 		?>
@@ -40,7 +40,7 @@ if ( ! wp_rig()->is_primary_nav_menu_active() ) {
 	$menu_toggle_button = '<button class="menu-toggle" aria-label="'. esc_html__( 'Open menu', 'wp-rig' ).'" aria-controls="primary-menu" aria-expanded="false">
 					'.esc_html__( 'Menu', 'wp-rig' ).'
 					</button>';
-	$menu_toggle_button = apply_filters('menu_toggle_button', $menu_toggle_button);
+	$menu_toggle_button = apply_filters('wp_rig_menu_toggle_button', $menu_toggle_button);
 	?>
 
 	<?php echo $menu_toggle_button; ?>
