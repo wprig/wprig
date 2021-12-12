e220785fa073a959a0a22e3fa6973347ef3cbf73
robruiz <rob.c.ruiz@gmail.com>
2021-12-12 10:32:20 -0600

    Undoing bad push to master

--- tests/phpunit/bootstrap-common.php
+++ tests/phpunit/bootstrap-common.php
@@ -13,6 +13,7 @@ if ( function_exists( 'xdebug_disable' ) ) {
 define( 'TESTS_THEME_DIR', dirname( __DIR__, 2 ) );
 define( 'TESTS_THEME_BASENAME', basename( TESTS_THEME_DIR ) );
 define( 'TESTS_THEME_URI', 'http://example.org/wp-content/themes/' . TESTS_THEME_BASENAME );
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', TESTS_THEME_DIR . '/vendor/yoast/phpunit-polyfils' );
 
 // Vendor directory is one level above 'dev' folder.
 $loader = require TESTS_THEME_DIR . '/vendor/autoload.php';
