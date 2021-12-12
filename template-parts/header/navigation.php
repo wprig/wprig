dc8f9f940721ca8490d374f9aa3f30f36ec50dee
robruiz <rob.c.ruiz@gmail.com>
2021-12-11 18:02:15 -0600

    Undoing bad push to master

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -36,14 +36,18 @@ if ( ! wp_rig()->is_primary_nav_menu_active() ) {
 	}
 	?>
 
-	<?php
-	$menu_toggle_button = '<button class="menu-toggle" aria-label="'. esc_html__( 'Open menu', 'wp-rig' ).'" aria-controls="primary-menu" aria-expanded="false">
-					'.esc_html__( 'Menu', 'wp-rig' ).'
-					</button>';
-	$menu_toggle_button = apply_filters('menu_toggle_button', $menu_toggle_button);
-	?>
-
-	<?php echo $menu_toggle_button; ?>
+	<button class="menu-toggle" aria-label="<?php esc_attr_e( 'Open menu', 'wp-rig' ); ?>" aria-controls="primary-menu" aria-expanded="false"
+		<?php
+		if ( wp_rig()->is_amp() ) {
+			?>
+			on="tap:AMP.setState( { siteNavigationMenu: { expanded: ! siteNavigationMenu.expanded } } )"
+			[aria-expanded]="siteNavigationMenu.expanded ? 'true' : 'false'"
+			<?php
+		}
+		?>
+	>
+		<?php esc_html_e( 'Menu', 'wp-rig' ); ?>
+	</button>
 
 	<div class="primary-menu-container">
 		<?php wp_rig()->display_primary_nav_menu( array( 'menu_id' => 'primary-menu' ) ); ?>
