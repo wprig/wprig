f086ebf9c7f44aec587bd393f6f5ea774c62a231
robruiz <rob.c.ruiz@gmail.com>
2024-11-03 20:05:37 -0600

    still working on eslint

--- assets/js/src/customizer.js
+++ assets/js/src/customizer.js
@@ -8,19 +8,19 @@
 
 ( function( $ ) {
 	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
+	window.wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-title a' ).text( to );
 		} );
 	} );
-	wp.customize( 'blogdescription', function( value ) {
+	window.wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
 		} );
 	} );
 
 	// Header text color.
-	wp.customize( 'header_textcolor', function( value ) {
+	window.wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
 				$( '.site-title, .site-description' ).css( {
