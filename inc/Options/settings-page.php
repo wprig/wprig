f0a12d8c04c355c609411e21fa71733595c70da8
robruiz <rob.c.ruiz@gmail.com>
2024-12-01 21:14:38 -0600

    cleanup with phpcbf

--- inc/Options/settings-page.php
+++ inc/Options/settings-page.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * Content to display in Theme Settings admin page.
+ * React loads in the app container div wp-rig-settings-page.
+ *
+ * @package wp_rig
+ */
+
 wp_enqueue_style(
-	'wp-components',
+	'wp-components'
 );
 ?>
 <div class="wrap">
-	<h1><?php esc_html_e( 'Theme Settings', 'my-theme' ); ?></h1>
+	<h1><?php esc_html_e( 'Theme Settings', 'wp-rig' ); ?></h1>
 	<div id="wp-rig-settings-page"></div>
 </div><?php
