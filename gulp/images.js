87c7fc8bb2f531e97588e697839f08da77080cff
robruiz <rob.c.ruiz@gmail.com>
2025-01-04 16:12:10 -0600

    updated version in styles.css

--- gulp/images.js
+++ gulp/images.js
@@ -11,34 +11,40 @@ import pump from 'pump';
  * Internal dependencies
  */
 import { paths } from './constants.js';
-import gulpImagemin from "gulp-imagemin";
-import gulpNewer from "gulp-newer";
+import gulpImagemin from 'gulp-imagemin';
+import gulpNewer from 'gulp-newer';
 import webp from 'gulp-webp';
 
 /**
  * Optimize images.
- * @param {function} done function to call when async processes finish
+ * @param {Function} done function to call when async processes finish
  * @return {Stream} single stream
  */
-export function images( done ) {
-	return pump( [
-		src( paths.images.src ),
-		gulpNewer( paths.images.dest ),
-		gulpImagemin(),
-		dest( paths.images.dest ),
-	], done );
+export function images(done) {
+	return pump(
+		[
+			src(paths.images.src),
+			gulpNewer(paths.images.dest),
+			gulpImagemin(),
+			dest(paths.images.dest),
+		],
+		done
+	);
 }
 
 /**
  * Convert images to webp.
- * @param {function} done function to call when async processes finish
+ * @param {Function} done function to call when async processes finish
  * @return {Stream} single stream
  */
-export function convertToWebP( done ) {
-	return pump( [
-		src( paths.images.src ),
-		gulpNewer( paths.images.dest ),
-		webp(),
-		dest( paths.images.dest ),
-	], done );
+export function convertToWebP(done) {
+	return pump(
+		[
+			src(paths.images.src),
+			gulpNewer(paths.images.dest),
+			webp(),
+			dest(paths.images.dest),
+		],
+		done
+	);
 }
