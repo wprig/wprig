2b17b639244fe3da9f792fa774842c173853d874
Jan Tack <jan.tack@digidrom.de>
2025-08-25 17:49:47 +0200

    Using auto fix to make js, jsx and ts files WPCS conform

--- assets/js/src/admin/index.jsx
+++ assets/js/src/admin/index.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
-import { useState, useCallback } from "react";
+import { createRoot } from 'react-dom/client';
+import { useState, useCallback } from 'react';
 import {
 	PanelRow,
 	TabPanel,
@@ -8,127 +8,142 @@ import {
 	BaseControl,
 	SelectControl,
 	FormToggle,
-} from "@wordpress/components";
-import { updateSettings } from "./api.js";
-import formFieldsData from "./settingsFields.json";
+} from '@wordpress/components';
+import { updateSettings } from './api.js';
+import formFieldsData from './settingsFields.json';
 
 // Debounce function to limit the frequency of calling a function
-const debounce = (func, wait) => {
+const debounce = ( func, wait ) => {
 	let timeout;
-	return (...args) => {
-		clearTimeout(timeout);
-		timeout = setTimeout(() => func(...args), wait);
+	return ( ...args ) => {
+		clearTimeout( timeout );
+		timeout = setTimeout( () => func( ...args ), wait );
 	};
 };
 
 const textControlTypes = [
-	"text",
-	"email",
-	"url",
-	"password",
-	"number",
-	"search",
-	"tel",
-	"date",
-	"time",
-	"datetime-local",
+	'text',
+	'email',
+	'url',
+	'password',
+	'number',
+	'search',
+	'tel',
+	'date',
+	'time',
+	'datetime-local',
 ];
 
 const SettingsPage = () => {
-	const [settings, setSettings] = useState(
-		window["wpRigThemeSettings"].settings,
+	const [ settings, setSettings ] = useState(
+		window.wpRigThemeSettings.settings
 	);
-	const [snackbarNotices, setSnackbarNotices] = useState([]);
+	const [ snackbarNotices, setSnackbarNotices ] = useState( [] );
 
 	const debouncedUpdateSettings = useCallback(
-		debounce((newSettings) => {
-			updateSettings(newSettings).then((response) => {
-				if (response.success) {
+		debounce( ( newSettings ) => {
+			updateSettings( newSettings ).then( ( response ) => {
+				if ( response.success ) {
 					const newSnackbarNotices = [
 						...snackbarNotices,
 						{
 							id: Date.now(),
-							content: "Settings saved!",
-							spokenMessage: "Settings saved!",
+							content: 'Settings saved!',
+							spokenMessage: 'Settings saved!',
 						},
 					];
-					setSnackbarNotices(newSnackbarNotices);
-					setTimeout(() => {
-						setSnackbarNotices((prevNotices) =>
+					setSnackbarNotices( newSnackbarNotices );
+					setTimeout( () => {
+						setSnackbarNotices( ( prevNotices ) =>
 							prevNotices.filter(
-								(notice) =>
-									notice.id !== newSnackbarNotices[0].id,
-							),
+								( notice ) =>
+									notice.id !== newSnackbarNotices[ 0 ].id
+							)
 						);
-					}, 2000);
+					}, 2000 );
 				} else {
-					console.error("Failed to save settings:", response);
+					console.error( 'Failed to save settings:', response );
 				}
-			});
-		}, 1500),
-		[snackbarNotices],
+			} );
+		}, 1500 ),
+		[ snackbarNotices ]
 	);
 
-	const handleChange = (settingKey, value) => {
-		const newSettings = { ...settings, [settingKey]: value };
-		setSettings(newSettings);
-		debouncedUpdateSettings(newSettings);
+	const handleChange = ( settingKey, value ) => {
+		const newSettings = { ...settings, [ settingKey ]: value };
+		setSettings( newSettings );
+		debouncedUpdateSettings( newSettings );
 	};
 
 	return (
 		<div className="settings-page">
 			<TabPanel
-				tabs={formFieldsData.tabs.map((tab) => ({
+				tabs={ formFieldsData.tabs.map( ( tab ) => ( {
 					name: tab.id,
-					title: tab["tabControl"].label,
-				}))}
+					title: tab.tabControl.label,
+				} ) ) }
 			>
-				{(tab) => (
+				{ ( tab ) => (
 					<div>
-						{formFieldsData.tabs
-							.find((t) => t.id === tab.name)["tabContent"].fields.map((field) => (
-								<PanelRow key={field.name}>
-									{field.type === "toggle" && (
-										<BaseControl label={field.label}>
+						{ formFieldsData.tabs
+							.find( ( t ) => t.id === tab.name )
+							.tabContent.fields.map( ( field ) => (
+								<PanelRow key={ field.name }>
+									{ field.type === 'toggle' && (
+										<BaseControl label={ field.label }>
 											<FormToggle
-												checked={!!settings[field.name]}
-												onChange={(event) =>
+												checked={
+													!! settings[ field.name ]
+												}
+												onChange={ ( event ) =>
 													handleChange(
 														field.name,
-														event.target.checked,
+														event.target.checked
 													)
 												}
 											/>
 										</BaseControl>
-									)}
-									{field.type === "select" && (
+									) }
+									{ field.type === 'select' && (
 										<SelectControl
 											__nextHasNoMarginBottom
-											label={field.label}
-											value={settings[field.name] || ""}
-											onChange={(value) =>
-												handleChange(field.name, value)
+											label={ field.label }
+											value={
+												settings[ field.name ] || ''
 											}
-											options={field.options}
+											onChange={ ( value ) =>
+												handleChange(
+													field.name,
+													value
+												)
+											}
+											options={ field.options }
 										/>
-									)}
-									{textControlTypes.includes(field.type) && (
+									) }
+									{ textControlTypes.includes(
+										field.type
+									) && (
 										<TextControl
-											label={field.label}
-											type={field.type}
-											value={settings[field.name] || ""}
-											onChange={(value) =>
-												handleChange(field.name, value)
+											label={ field.label }
+											type={ field.type }
+											value={
+												settings[ field.name ] || ''
+											}
+											onChange={ ( value ) =>
+												handleChange(
+													field.name,
+													value
+												)
 											}
 										/>
-									)}
+									) }
 								</PanelRow>
-							))}
+							) ) }
 					</div>
-				)}
+				) }
 			</TabPanel>
 			<div id="settings-saved">
-				<SnackbarList notices={snackbarNotices} />
+				<SnackbarList notices={ snackbarNotices } />
 			</div>
 		</div>
 	);
@@ -137,11 +152,11 @@ const SettingsPage = () => {
 export default SettingsPage;
 
 const renderSettingsPage = () => {
-	const container = document.getElementById("wp-rig-settings-page");
-	if (container) {
-		const root = createRoot(container);
-		root.render(<SettingsPage />);
+	const container = document.getElementById( 'wp-rig-settings-page' );
+	if ( container ) {
+		const root = createRoot( container );
+		root.render( <SettingsPage /> );
 	}
 };
 
-document.addEventListener("DOMContentLoaded", renderSettingsPage);
+document.addEventListener( 'DOMContentLoaded', renderSettingsPage );
