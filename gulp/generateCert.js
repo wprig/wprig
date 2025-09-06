1e13457c84614bfeb4408610760ab34e447eae9c
Jan Tack <jan.tack@digidrom.de>
2025-09-06 18:35:50 +0200

    fixes auto-fixable linting errors in the gulp folder

--- gulp/generateCert.js
+++ gulp/generateCert.js
@@ -61,7 +61,11 @@ export default function generateCert( done ) {
 			}
 		} );
 
-		log( colors.green( 'Custom SSL key and certificate generated successfully!' ) );
+		log(
+			colors.green(
+				'Custom SSL key and certificate generated successfully!'
+			)
+		);
 
 		done();
 	} );
